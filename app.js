661061ab4f6c4e05c38febf0ef13f2ecd4a1b555
Lily M. <withlove.lilya@gmail.com>
2026-07-17 20:11:33 +0300

    add adaptation for different device, changed dark theme inage, changed colors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 'use strict';
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function() {
+const switcher = document.getElementById('theme-switcher');
+
+if(switcher){
+    switcher.addEventListener('click', function() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
-    const className = document.body.className;
-    if(className == "light-theme") {
+    const isDark = document.body.classList.contains('sark-theme');
+    if(isDark) {
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
@@ -13,3 +15,4 @@ switcher.addEventListener('click', function() {
 
     console.log('current class name: ' + className);
 });
+}
