3563d9c77e99ab448c4d88623c9d61162ac235e8
Lily M. <withlove.lilya@gmail.com>
2026-07-17 20:58:14 +0300

    Code update

--- app.js
+++ app.js
@@ -2,17 +2,15 @@
 const switcher = document.getElementById('theme-switcher');
 
 if(switcher){
-    switcher.addEventListener('click', function() {
+    switcher.addEventListener('click', function(){
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
-    const isDark = document.body.classList.contains('sark-theme');
+    const isDark = document.body.classList.contains('dark-theme');
     if(isDark) {
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
     }
-
-    console.log('current class name: ' + className);
 });
 }
